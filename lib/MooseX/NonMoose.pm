c89ec958fc7dceb47e7f097ca76cce104e5dde77
doy <doy@tozt.net>
2009-04-09 01:56:09 -0500

    factor the constructor out into a separate sub

--- lib/MooseX/NonMoose.pm
+++ lib/MooseX/NonMoose.pm
@@ -6,6 +6,20 @@ Moose::Exporter->setup_import_methods(
     with_caller => ['extends_nonmoose'],
 );
 
+sub constructor {
+    my $class = shift;
+    my $meta = Class::MOP::Class->initialize($class);
+    my $super_new = $meta->find_next_method_by_name('new');
+    my $self = $super_new->execute($class, @_);
+    my $params = $class->BUILDARGS(@_);
+    my $moose_self = $meta->new_object(
+        __INSTANCE__ => $self,
+        %$params,
+    );
+    $moose_self->BUILDALL($params);
+    return $moose_self;
+}
+
 sub extends_nonmoose {
     my $caller = shift;
     my @superclasses = @_;
@@ -15,19 +29,7 @@ sub extends_nonmoose {
 
     Moose::extends($caller, @superclasses);
 
-    Class::MOP::Class->initialize($caller)->add_method(new => sub {
-        my $class = shift;
-        my $meta = Class::MOP::Class->initialize($class);
-        my $super_new = $meta->find_next_method_by_name('new');
-        my $self = $super_new->execute($class, @_);
-        my $params = $class->BUILDARGS(@_);
-        my $moose_self = $meta->new_object(
-            __INSTANCE__ => $self,
-            %$params,
-        );
-        $moose_self->BUILDALL($params);
-        return $moose_self;
-    });
+    Class::MOP::Class->initialize($caller)->add_method(new => \&constructor);
 }
 
 sub init_meta {
