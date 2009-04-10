905935092f4be848a98e186a8b7009e220365d9b
doy <doy@tozt.net>
2009-04-10 02:17:05 -0500

    hmmm, this conditional wasn't doing anything and tests still pass, so...? probably look more into this

--- lib/MooseX/NonMoose.pm
+++ lib/MooseX/NonMoose.pm
@@ -15,29 +15,26 @@ sub extends_nonmoose {
 
     Moose::extends($caller, @superclasses);
 
-    my $meta = Class::MOP::Class->initialize($caller);
-    if ($meta->find_next_method_by_name('new')->body ne \&constructor) {
-        $meta->add_method(new => sub {
-            my $class = shift;
-            my $meta = Class::MOP::Class->initialize($class);
+    my $caller_meta = Class::MOP::Class->initialize($caller);
+    $caller_meta->add_method(new => sub {
+        my $class = shift;
+        my $meta = Class::MOP::Class->initialize($class);
 
-            # we need to get the non-moose constructor from the superclass
-            # of the class where this method actually exists
-            my $caller_meta = Class::MOP::Class->initialize($caller);
-            my $super_new = $caller_meta->find_next_method_by_name('new');
-            # but we need to call it as a method on the class we're actually
-            # trying to instantiate
-            my $self = $super_new->execute($class, @_);
+        # we need to get the non-moose constructor from the superclass
+        # of the class where this method actually exists
+        my $super_new = $caller_meta->find_next_method_by_name('new');
+        # but we need to call it as a method on the class we're actually
+        # trying to instantiate
+        my $self = $super_new->execute($class, @_);
 
-            my $params = $class->BUILDARGS(@_);
-            my $moose_self = $meta->new_object(
-                __INSTANCE__ => $self,
-                %$params,
-            );
-            $moose_self->BUILDALL($params);
-            return $moose_self;
-        });
-    }
+        my $params = $class->BUILDARGS(@_);
+        my $moose_self = $meta->new_object(
+            __INSTANCE__ => $self,
+            %$params,
+        );
+        $moose_self->BUILDALL($params);
+        return $moose_self;
+    });
 }
 
 sub init_meta {
