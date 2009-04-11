becc48932fe476fc40bf5bf913566e310c5d2fd3
doy <doy@tozt.net>
2009-04-11 00:06:28 -0500

    oops, the failure was because it should actually be constructor_class_roles

--- lib/MooseX/NonMoose.pm
+++ lib/MooseX/NonMoose.pm
@@ -50,9 +50,9 @@ sub init_meta {
     my %options = @_;
     Moose->init_meta(%options);
     Moose::Util::MetaRole::apply_metaclass_roles(
-        for_class                   => $options{for_class},
-        metaclass_roles             => ['MooseX::NonMoose::Meta::Role::Class'],
-        constructor_metaclass_roles =>
+        for_class               => $options{for_class},
+        metaclass_roles         => ['MooseX::NonMoose::Meta::Role::Class'],
+        constructor_class_roles =>
             ['MooseX::NonMoose::Meta::Role::Constructor'],
     );
     return $options{for_class}->meta;
