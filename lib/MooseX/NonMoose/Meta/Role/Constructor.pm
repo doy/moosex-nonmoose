08fad75a0f6236c337bbd28d7a629be902e40237
doy <doy@tozt.net>
2009-04-20 21:04:34 -0500

    better rationale for not messing with the meta-instance api right now

--- lib/MooseX/NonMoose/Meta/Role/Constructor.pm
+++ lib/MooseX/NonMoose/Meta/Role/Constructor.pm
@@ -22,8 +22,9 @@ sub _generate_instance {
     my $new = $self->name;
     my $super_new_class = $self->associated_metaclass->find_next_method_by_name($new)->package_name;
     # XXX: this should probably be taking something from the meta-instance api,
-    # rather than calling bless directly, but all it can do at the moment is
-    # generate fresh instances
+    # rather than calling bless directly, but this works fine for now, and i
+    # want to wait for the whole immutablization stuff to settle down before
+    # digging too deeply into it
     "my $var = bless $super_new_class->$new(\@_), $class_var;\n";
 }
 
